e06ebbb145daaa521d2cd3c9462a71bfd3247e34
Jeff Ryan <jeff.ryan@appfire.com>
2021-04-19 13:47:06 -0500

    fix registry, unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Vaccine Locator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Vaccine Locator/i);
   expect(linkElement).toBeInTheDocument();
 });
